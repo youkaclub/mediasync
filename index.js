f7eb4dbaed2b8d355c2c049c3bde50c3650641a0
youka <youka.club@gmail.com>
2020-01-26 02:13:08 +0200

    fix isSafari and remove enableAudio

--- index.js
+++ index.js
@@ -19,23 +19,10 @@ class MediaSync {
     this.seekingFn = this.seeking.bind(this)
     this.volumechangeFn = this.volumechange.bind(this)
     this.timeupdateFn = this.timeupdate.bind(this)
-    this.enableAudioFn = this.enableAudio.bind(this)
-
-    if (this.isSafari()) {
-      document.addEventListener('touchstart', this.enableAudioFn)
-    }
   }
 
   isSafari () {
-    return window.navigator.userAgent.match(/(iPad|iPhone|iPod|Safari)/i)
-  }
-
-  enableAudio () {
-    document.removeEventListener('touchstart', this.enableAudioFn)
-    this.muted = true
-    this.medias.map(m => { m.play() })
-    this.medias.map(m => { m.pause() })
-    this.muted = false
+    return window.navigator.userAgent.match(/(iPad|iPhone|iPod|Safari)/i) !== null
   }
 
   add (media) {
@@ -45,7 +32,7 @@ class MediaSync {
     media.addEventListener('volumechange', this.volumechangeFn)
     media.addEventListener('timeupdate', this.timeupdateFn)
     const sync = mediaSync(media, this.to)
-    if (this.isSafari) {
+    if (this.isSafari()) {
       sync.setOption('target', 0.5)
     }
     this.syncs.push(sync)
